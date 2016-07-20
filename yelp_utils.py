5305fd2678041023bd8fb05366b90be1fc5159f8
Hrushikesh Dhumal <hrushikesh.dhumal@gmail.com>
2016-07-20 16:45:17 -0400

    added comments to NLP functions

--- yelp_utils.py
+++ yelp_utils.py
@@ -27,7 +27,7 @@ YELP_DATA_SPARSE_MATRIX_DIR = os.path.join(WORK_DIR, "data", "sparse_matrix")
 pd.options.display.max_columns = 200
 
 
-#FILE READ WRITE
+### FILE READ WRITE
 def make_sure_path_exists(path): 
     '''
     Function to  make a new directory structure if it doesnt exists.
@@ -80,17 +80,28 @@ def load_sparse_csr(filename):
     return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
                          shape = loader['shape'])
 
-#NLP FUNCTIONS:
+### NLP
+# The maps have been generated following -
 # http://stackoverflow.com/questions/11692199/string-translate-with-unicode-data-in-python
 remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 remove_number_map = dict((ord(char), None) for char in string.digits)
 
 def remove_numbers_in_string(s):
+    '''
+    Function to remove numbers in a string.
+    Input: string
+    Output: string
+    '''
 #     print(type(s))
 #     return s.translate(None, string.digits)
     return s.translate(remove_number_map)
 
 def lowercase_remove_punctuation(s):
+    '''
+    Function to lowercase string and remove punctuation marks
+    Input: string
+    Output: string
+    '''
     s = s.lower()
 #     s = s.translate(None, string.punctuation)
     return s.translate(remove_punctuation_map)
@@ -98,22 +109,54 @@ def lowercase_remove_punctuation(s):
 NLTK_STOPWORDS = set(stopwords.words('english'))
 
 def remove_stopwords(s):
+    '''
+    Function to remove stopwords. Stopwords list is used from NLTK package.
+    Source: 
+    https://github.com/kevin11h/YelpDatasetChallengeDataScienceAndMachineLearningUCSD/blob/master/Yelp%20Predictive%20Analytics.ipynb
+    Input: string 
+    Output: string
+    '''    
     token_list = nltk.word_tokenize(s)
     exclude_stopwords = lambda token : token not in NLTK_STOPWORDS
     return ' '.join(filter(exclude_stopwords, token_list))
 
-def filter_out_more_stopwords(token_list):
+def filter_out_more_stopwords(token_list, MORE_STOPWORDS):
+    '''
+    Function to filter out more stopwords
+    Source:
+    https://github.com/kevin11h/YelpDatasetChallengeDataScienceAndMachineLearningUCSD/blob/master/Yelp%20Predictive%20Analytics.ipynb
+    Input: 
+        token_list: list of words
+        MORE_STOPWORDS: list of stopwords
+    Output: 
+        list without stopwords
+    '''    
     return filter(lambda tok : tok not in MORE_STOPWORDS, token_list)
 
 def stem_token_list(token_list):
+    '''
+    Function to stem words
+    Input: list 
+    Output: list 
+    '''    
     STEMMER = PorterStemmer()
 #     return [STEMMER.stem(tok.decode('utf-8')) for tok in token_list]
     return [STEMMER.stem(tok) for tok in token_list]
 
 def restring_tokens(token_list):
+    '''
+    Function to convert the the tokenized words to string
+    Input: list
+    Output: string
+    '''    
     return ' '.join(token_list)
 
-def lowercase_remove_punctuation_and_numbers_and_tokenize_and_filter_more_stopwords_and_stem_and_restring(s):
+def lowercase_and_remove_punctuation_and_remove_numbers_and_tokenize_stem_and_restring(s):
+    '''
+    Function to lowercase, remove punctuation, remove numbers, stem each token in a string
+    Input: string
+    Output: string
+    '''    
     s = remove_numbers_in_string(s)
     s = lowercase_remove_punctuation(s)
     s = remove_stopwords(s)
